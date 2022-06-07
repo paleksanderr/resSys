19f9ebc51f3eb991e9fa554da778d02e88739e00
Pawel <pawel.aleksander@gmail.com>
2022-06-07 12:13:09 +0200

    progress

--- src/Component/Hotels/Hotel/Hotel.js
+++ src/Component/Hotels/Hotel/Hotel.js
@@ -1,9 +1,11 @@
 import React from "react";
-import styles from './hotel.module.css'
+import styles from "./hotel.module.css";
+import hotelImg from "../../../assets/images/pexels-jeffrey-czum-2501965.jpg";
 
-export default function Hotel(){
+export default function Hotel() {
   return (
     <div className={`${styles.hotel} `}>
+      <img alt="" src={hotelImg} className="img-fluid "></img>
       <h4>Hotel</h4>
     </div>
   );
